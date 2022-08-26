9908b545b6524b9e54dd89dc7fd47752484c27ea
Alexander Golden <felix@agolden.com>
2022-08-26 15:11:14 -0400

    fix vercel compile errors

--- pages/events.js
+++ pages/events.js
@@ -25,37 +25,7 @@ const Events = ({ events }) => {
 	// console.log(events);
 	return (
 		<>
-			<NextSeo
-				title="Events | Cambridge Community Kitchen"
-				description="Upcoming events and fundraisers from Cambridge Community Kitchen"
-				openGraph={{
-					title: 'Events | Cambridge Community Kitchen',
-					description:
-						'Upcoming events and fundraisers from Cambridge Community Kitchen',
-					images: [{ url: 'https://cckitchen.uk/cck-preview.png' }],
-					url: 'https://cckitchen.uk/events',
-					type: 'website',
-				}}
-			/>
-			<SafeHydrate>
-				<LayoutContainer>
-					<Flex justifyContent="center">
-						<Container
-							maxWidth={useBreakpointValue({ base: '90%', md: '750px' })}
-							mb={8}
-						>
-							<Heading as="h1" mb={8}>
-								Events
-							</Heading>
-							<VStack spacing={12}>
-								{events.map((event) => (
-									<Card event={event} key={event.id} />
-								))}
-							</VStack>
-						</Container>
-					</Flex>
-				</LayoutContainer>
-			</SafeHydrate>
+			TBD
 		</>
 	);
 };

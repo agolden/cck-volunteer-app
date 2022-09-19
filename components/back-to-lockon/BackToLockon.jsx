e25b96b2aad22abad2b4de5c0b46d2ff12c3ec7c
Alexander Golden <felix@agolden.com>
2022-09-19 16:29:47 -0400

    Fix missing pem fil

--- components/back-to-lockon/BackToLockon.jsx
+++ components/back-to-lockon/BackToLockon.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import { Flex, Button } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 const BackToLockon = () => {
 	const encodedGoogleMapsUrl = `https://goo.gl/maps/TKn359eKxHWskMJV6`; // The Lockon
 //	const encodedGoogleMapsUrl = `https://goo.gl/maps/yURo7vxnViPK3ngf7`; // Downing Place
 	return (
-		<Box d="flex" alignItems="center" justifyContent="center" py={5}>
+		<Flex direction="row" py={5}>
 			<Button
 				as="a"
+				margin="auto"
 				href={encodedGoogleMapsUrl}
 				colorScheme="yellow"
 				rightIcon={<ArrowForwardIcon />}
@@ -17,7 +18,7 @@ const BackToLockon = () => {
 			>
 				Back to the Lockon
 			</Button>
-		</Box>
+		</Flex>
 	);
 };
 
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Header = () => (
 	<div>
-		<Box d="flex" display="flex" alignItems="center" justifyContent="center" mx="4" my="6">
+		<Box display="flex" alignItems="center" justifyContent="center" mx="4" my="6">
 			<Image margin="auto" alt="CCK Logo" src="/logo.png" height={50} width={50} />
 			<Heading ml={2}>CCK Deliveries</Heading>
 		</Box>

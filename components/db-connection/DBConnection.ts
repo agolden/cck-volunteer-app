01304ef11b1ee5f0f055f63d2e9d4eb99a3350df
Alexander Golden <felix@agolden.com>
2022-09-19 14:14:26 -0400

    Move AWS cert to constant

--- components/db-connection/DBConnection.ts
+++ components/db-connection/DBConnection.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 import util from 'util';
 import SQL from 'sql-template-strings';
-import * as fs from 'fs';
+import { awsPem } from './DBHelpers';
 
 const dbConfig = {
     connectionLimit : 100, //important
@@ -12,8 +12,9 @@ const dbConfig = {
 };
 
 if (process.env.NO_DB_SSL !== "true" && process.env.DB_HOST.includes('aws.com')){
+	const awsdec = Buffer.from(awsPem, 'base64').toString('utf8').replace(/\\n/g, "\n");
 	dbConfig['ssl'] = {
-		ca : fs.readFileSync(process.cwd() + "/aws/eu-west-2-bundle.pem")
+		ca : awsdec
 	};
 }
 

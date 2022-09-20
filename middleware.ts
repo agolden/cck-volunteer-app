a3cb92f4c985971fcc8ed05e32a2305fac962f89
Alexander Golden <felix@agolden.com>
2022-09-20 05:54:30 -0400

    Add rate limiting to totp requests

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,30 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { jwtVerify } from 'jose';
 import { getUserContext } from '@/components/api-helpers';
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 export async function middleware(request: NextRequest) {
+
+  const rateLimited = [
+    /^\/api\/auth\/totp\/request/
+  ];
+  
+  if(rateLimited.some(rx => rx.test(request.nextUrl.pathname)) && process.env.UPSTASH_REDIS_REST_URL) {
+
+    // You can only request a totp up to 25 times a day from a single IP address.
+    const ratelimit = new Ratelimit({
+      redis: Redis.fromEnv(),
+      limiter: Ratelimit.slidingWindow(25, "1 d"),
+    });
+
+    const identifier = request.ip ? request.ip : "127.0.0.1"
+    console.log(identifier);
+    const { success } = await ratelimit.limit(identifier);
+    if (!success) {
+      return NextResponse.rewrite(new URL('/api/auth/unauthorized', request.url));
+    }
+  }
 
   const allowedWithoutAuth = [
     /^\/log(in|out)$/,
